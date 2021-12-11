74496f78289771d752724a9e1308227a5e373d4e
Belsch, Marc (FIE4_EXTERN) <extern.marc.belsch@porsche.de>
2021-12-11 17:31:39 +0100

    Provided the neccessary sql files

---
+++ psql/script01-dbcreation.sql
@@ -0,0 +1,11 @@
+﻿DROP ROLE IF EXISTS gamification;
+CREATE ROLE gamification WITH SUPERUSER ENCRYPTED PASSWORD 'gamification';
+ALTER ROLE gamification WITH LOGIN;
+
+DROP DATABASE IF EXISTS gamification;
+
+CREATE DATABASE gamification;
+
+GRANT CONNECT, TEMPORARY ON DATABASE gamification TO gamification;
+GRANT ALL ON DATABASE gamification TO gamification;
+
